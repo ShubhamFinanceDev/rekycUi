fc05cd905815f9d954aa5d72df9868f0a5a9081c
nainish <pmo_dbalounge@shubham.co>
2024-06-06 14:56:26 +0530

    public url

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        // BASE_URL: "http://10.16.1.131:8070"  //prod url
-        // BASE_URL: "http://10.12.12.39:8070"
-        // BASE_URL: "http://localhost:8070/"
-        // BASE_URL:"http://144.24.96.140:8070"
-        // BASE_URL: "http://10.16.1.46:8070"
-        BASE_URL:"https//152.67.24.187:8443"
+        BASE_URL:"https//152.67.24.187:8443" //prod url
     }
 }
 
