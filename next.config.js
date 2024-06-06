b4cfdf6a949d91e8c69f28b5eb5ea17a252da5d9
kajalthakur99 <kajal.01@dbalounge.com>
2024-06-06 14:48:24 +0530

    adding public url

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
         // BASE_URL: "http://10.12.12.39:8070"
         // BASE_URL: "http://localhost:8070/"
         // BASE_URL:"http://144.24.96.140:8070"
-        BASE_URL: "http://10.16.1.46:8070"
+        // BASE_URL: "http://10.16.1.46:8070"
+        BASE_URL:"https//152.67.24.187:8443"
     }
 }
 
